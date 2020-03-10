2921eb9b1105641678c5a7e1e558b853cf326306
Zhuotang Li <ZT-Li@users.noreply.github.com>
2020-03-10 15:32:41 -0700

    userInput updates

--- user input/loop.js
+++ user input/loop.js
@@ -1,8 +1,7 @@
-window.onload = function() {
-  let oBox = document.getElementById("d1");
-  let oColor = document.getElementById("color");
-  alert(oColor);
-  function setColor(oColor) {
-    oBox.style.backgroundColor = oColor;
-  }
-};
+let oBox = document.getElementById("d1");
+let oColor = document.getElementsById("color");
+alert(oColor);
+function setColor(oColor) {
+  oBox.style.backgroundColor = oColor;
+}
+
