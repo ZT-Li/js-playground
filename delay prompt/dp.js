ced5bc6bcb1d6837578e3f98aa45b68e7ab61b4b
Zhuotang Li <ZT-Li@users.noreply.github.com>
2020-03-18 16:09:57 -0700

    delay prompt

--- delay prompt/dp.js
+++ delay prompt/dp.js
@@ -1,9 +1,13 @@
 window.onload = function () {
     let oIcon = document.getElementById("icon");
     let oInfo = document.getElementById("info");
+
+    //show the info tab
     oIcon.onmouseover = function () {
         oInfo.style.visibility = "visible";
     }
+
+    //hide the info tab
     oIcon.onmouseleave = function () {
         oInfo.style.visibility = "hidden";
     }
