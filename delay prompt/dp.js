096a0cd654725da60b7456424e46a34eed64aab7
Zhuotang Li <ZT-Li@users.noreply.github.com>
2020-03-23 20:01:38 -0700

    dp update

--- delay prompt/dp.js
+++ delay prompt/dp.js
@@ -1,14 +1,31 @@
 window.onload = function () {
     let oIcon = document.getElementById("icon");
     let oInfo = document.getElementById("info");
+    let timer = null;
 
-    //show the info tab
+    //show the info tab when mouse over icon
     oIcon.onmouseover = function () {
-        oInfo.style.visibility = "visible";
+        clearTimeout(timer);
+        oInfo.style.display = "block";
     }
 
-    //hide the info tab
-    oIcon.onmouseleave = function () {
-        oInfo.style.visibility = "hidden";
+    //close the info tab after 1 sec the mouse leave
+    oIcon.onmouseout = oInfo.onmouseout = function () {
+        timer = setTimeout(function () {
+            oInfo.style.display = "none";
+        }, 1000);
+    }
+
+    //show the info tab when mouse over info
+    oInfo.onmouseover = function () {
+        clearTimeout(timer);
+        oInfo.style.display = "block";
+    }
+
+    //close the info tab after 1 sec the mouse leave
+    oInfo.onmouseout = function () {
+        timer = setTimeout(function () {
+            oInfo.style.display = "none";
+        }, 1000);
     }
 }
