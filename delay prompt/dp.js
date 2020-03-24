9bc0f5fdc7b1093296d2d9622dc2ad9676481128
Zhuotang Li <ZT-Li@users.noreply.github.com>
2020-03-23 20:08:53 -0700

    dp update + code optimize

--- delay prompt/dp.js
+++ delay prompt/dp.js
@@ -3,27 +3,14 @@ window.onload = function () {
     let oInfo = document.getElementById("info");
     let timer = null;
 
-    //show the info tab when mouse over icon
-    oIcon.onmouseover = function () {
+    //show the info tab when mouse over icon and info tab
+    oIcon.onmouseover = oInfo.onmouseover = function () {
         clearTimeout(timer);
         oInfo.style.display = "block";
     }
 
     //close the info tab after 1 sec the mouse leave
     oIcon.onmouseout = oInfo.onmouseout = function () {
-        timer = setTimeout(function () {
-            oInfo.style.display = "none";
-        }, 1000);
-    }
-
-    //show the info tab when mouse over info
-    oInfo.onmouseover = function () {
-        clearTimeout(timer);
-        oInfo.style.display = "block";
-    }
-
-    //close the info tab after 1 sec the mouse leave
-    oInfo.onmouseout = function () {
         timer = setTimeout(function () {
             oInfo.style.display = "none";
         }, 1000);
