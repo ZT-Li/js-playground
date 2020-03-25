932030bb207cadf11429cf2d070e4a2f42c72615
Zhuotang Li <ZT-Li@users.noreply.github.com>
2020-03-24 20:51:21 -0700

    fade out in progress...

--- delay prompt/dp.js
+++ delay prompt/dp.js
@@ -2,17 +2,29 @@ window.onload = function () {
     let oIcon = document.getElementById("icon");
     let oInfo = document.getElementById("info");
     let timer = null;
+    let opc = 1;
 
     //show the info tab when mouse over icon and info tab
     oIcon.onmouseover = oInfo.onmouseover = function () {
-        clearTimeout(timer);
+        //clearTimeout(timer);
+        clearInterval(timer);
+        opc = 1;
+        oInfo.style.opacity = opc;
         oInfo.style.display = "block";
     }
 
     //close the info tab after 1 sec the mouse leave
     oIcon.onmouseout = oInfo.onmouseout = function () {
-        timer = setTimeout(function () {
+        // timer = setTimeout(function () {
+        //     oInfo.style.display = "none";
+        // }, 1000);
+        if (opc == 0)
             oInfo.style.display = "none";
-        }, 1000);
+        else {
+            timer = setInterval(function () {
+                opc -= 0.01;
+                oInfo.style.opacity = opc;
+            }, 10)
+        }
     }
 }
