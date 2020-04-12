1f5d08c41616e33e656f9585e1d71b5e16fa1639
Zhuotang Li <zt.li.fisher@gmail.com>
2020-04-12 16:56:06 -0700

    dp bug fixed

--- delay prompt/dp.js
+++ delay prompt/dp.js
@@ -1,30 +1,26 @@
 window.onload = function () {
-    let oIcon = document.getElementById("icon");
-    let oInfo = document.getElementById("info");
-    let timer = null;
+  let oIcon = document.getElementById("icon");
+  let oInfo = document.getElementById("info");
+  let timer = null;
+  let timer2 = null;
+
+  //show the info tab when mouse over icon and info tab
+  oIcon.onmouseover = oInfo.onmouseover = function () {
+    clearTimeout(timer);
+    clearInterval(timer2);
+    oInfo.style.opacity = 1;
+    oInfo.style.display = "block";
+  };
+
+  //close the info tab after 1 sec the mouse leave
+  oIcon.onmouseout = oInfo.onmouseout = function () {
     let opc = 1;
-
-    //show the info tab when mouse over icon and info tab
-    oIcon.onmouseover = oInfo.onmouseover = function () {
-        //clearTimeout(timer);
-        clearInterval(timer);
-        opc = 1;
-        oInfo.style.opacity = opc;
-        oInfo.style.display = "block";
-    }
-
-    //close the info tab after 1 sec the mouse leave
-    oIcon.onmouseout = oInfo.onmouseout = function () {
-        // timer = setTimeout(function () {
-        //     oInfo.style.display = "none";
-        // }, 1000);
-        if (opc == 0)
-            oInfo.style.display = "none";
-        else {
-            timer = setInterval(function () {
-                opc -= 0.01;
-                oInfo.style.opacity = opc;
-            }, 10)
-        }
-    }
-}
+    timer = setTimeout(function () {
+      oInfo.style.display = "none";
+    }, 1000);
+    timer2 = setInterval(function () {
+      opc -= 0.01;
+      oInfo.style.opacity = opc;
+    }, 10);
+  };
+};
